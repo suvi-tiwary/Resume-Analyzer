005509a5866332dc2f6f42f4b13b864a2fb0d6c2
Suvi Tiwary <suvikumari345@gmail.com>
2026-09-21 22:48:00 +0530

    Fix frontend localhost origin

--- AI/main.py
+++ AI/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 origins = [
     # Local development
     "http://localhost:3000",
-    "http://localhost:5173",
+    "http://localhost:5173/",
 
     # Firebase Hosting
     "https://resume-analyser-3195c.web.app"
