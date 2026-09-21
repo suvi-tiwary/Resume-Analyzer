dd03d4031887f4ec33b05c99902d677702d23bb4
Suvi Tiwary <suvikumari345@gmail.com>
2026-09-21 18:13:26 +0530

    1

--- AI/main.py
+++ AI/main.py
@@ -4,7 +4,12 @@ import tempfile
 
 from Resume_loaders import extract_resume
 from Resume_parser import resume_parse
+import os
+import uvicorn
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 app = FastAPI()
 
